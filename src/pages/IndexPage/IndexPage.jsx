a2451cb04f2bdbf7928e3a017167910c6bfd75bd
Nathan Santiago <santiagonathan9254@gmail.com>
2024-06-27 01:44:08 -0400

    Create spline on homepage. Add description to Ram Eats.

--- src/pages/IndexPage/IndexPage.jsx
+++ src/pages/IndexPage/IndexPage.jsx
@@ -1,52 +1,63 @@
 import './IndexPage.scss'
 
+import Spline from '@splinetool/react-spline';
+
 export default function IndexPage() {
     return (
         <div className='text-zone'>
-            <h1 className='introduction'>
-                <div className='line'>
-                    <span className='word'>
-                        <span className="letter">H</span>
-                        <span className="letter">i</span>
-                        <span className="letter">,</span>
-                    </span>
+            <div className="index-container">
+                <div className="text-container">
+                    <h1 className='introduction'>
+                        <div className='line'>
+                            <span className='word'>
+                                <span className="letter">H</span>
+                                <span className="letter">i</span>
+                                <span className="letter">,</span>
+                            </span>
+                        </div>
+                        <div className="line">
+                            <span className='word'>
+                                <span className="letter">I</span>
+                                <span className="letter">'</span>
+                                <span className="letter">m</span>
+                            </span>
+                            <span className='word'>
+                                <span className="letter">N</span>
+                                <span className="letter">a</span>
+                                <span className="letter">t</span>
+                                <span className="letter">h</span>
+                                <span className="letter">a</span>
+                                <span className="letter">n</span>
+                            </span>
+                        </div>
+                        <div className="line">
+                            <span className='word'>
+                                <span className="letter">a</span>
+                            </span>
+                            <span className='word'>
+                                <span className="letter">d</span>
+                                <span className="letter">e</span>
+                                <span className="letter">v</span>
+                                <span className="letter">e</span>
+                                <span className="letter">l</span>
+                                <span className="letter">o</span>
+                                <span className="letter">p</span>
+                                <span className="letter">e</span>
+                                <span className="letter">r</span>
+                                <span className="letter">.</span>
+                            </span>
+                        </div>
+                    </h1>
+                    <h2 className='subheader'>
+                        Fullstack developer
+                    </h2>
                 </div>
-                <div className="line">
-                    <span className='word'>
-                        <span className="letter">I</span>
-                        <span className="letter">'</span>
-                        <span className="letter">m</span>
-                    </span>
-                    <span className='word'>
-                        <span className="letter">N</span>
-                        <span className="letter">a</span>
-                        <span className="letter">t</span>
-                        <span className="letter">h</span>
-                        <span className="letter">a</span>
-                        <span className="letter">n</span>
-                    </span>
+                <div className="spline-container">
+                    <Spline
+                        scene="https://prod.spline.design/aP4cotlzZdRz9F6U/scene.splinecode" className='disable-spline'
+                    />
                 </div>
-                <div className="line">
-                    <span className='word'>
-                        <span className="letter">a</span>
-                    </span>
-                    <span className='word'>
-                        <span className="letter">d</span>
-                        <span className="letter">e</span>
-                        <span className="letter">v</span>
-                        <span className="letter">e</span>
-                        <span className="letter">l</span>
-                        <span className="letter">o</span>
-                        <span className="letter">p</span>
-                        <span className="letter">e</span>
-                        <span className="letter">r</span>
-                        <span className="letter">.</span>
-                    </span>
-                </div>
-            </h1>
-            <h2 className='subheader'>
-                Fullstack developer
-            </h2>
+            </div>
         </div>
     );
 }
--- src/pages/PortfolioPage/PortfolioPage.jsx
+++ src/pages/PortfolioPage/PortfolioPage.jsx
@@ -14,7 +14,7 @@ import { useState } from 'react';
 export default function PortfolioPage() {
 
     const descriptionList = [
-        'Ram Eats is an app',
+        'RamEats is an app that will revolutionize your dining experience at Chase and Lenoir. RamEats will be a user-friendly mobile application designed to provide UNC students with daily food recommendations tailored to their fitness goals. Whether you\'re aiming to lose weight, gain muscle, or simply maintain a healthy lifestyle, RamEats will be able to provide dining hall specific nutrition recommendations. Users will start by entering their fitness goals into the app. RamEats will personalize the user\'s experience based on their objectives. \nNo more guesswork or endless scrolling through menus with a bad UI! RamEats will ensure that every bite you take aligns with your fitness aspirations. Users will be able to keep tabs on their journey towards better health with a progress tracking feature, and monitor their calorie intake, macronutrient distribution, and more to stay on course. While RamEats provides recommendations, it also empowers you to make informed decisions about your diet. With access to nutritional information and diverse meal options, you\'re in control of your food choices like never before.',
         'SafeCuisine is an app',
         'Agricultural Water Usage Calculator',
         'CDC 2023',
@@ -45,9 +45,12 @@ export default function PortfolioPage() {
                             </span>
                         </div>
                     </h1>
-                    <p>
-                        {photoDescription}
-                    </p>
+                    {/* this arrow function creates a line break at each \n */}
+                    {photoDescription.split("\n").map((string) =>
+                        <p>
+                            {photoDescription}
+                        </p>
+                    )}
                 </div>
                 <div className='gallery-container'>
                     <Swiper
